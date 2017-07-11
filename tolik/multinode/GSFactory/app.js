43958041ff02756fc35ce8b25c43bb5bc7b29c10
aokhotnikov <aokhotnikov@softjourn.com>
2017-07-11 10:45:28 +0300

    Test app for multinode

---
+++ tolik/multinode/GSFactory/app.js
@@ -0,0 +1,39 @@
+// requires
+var fs = require ('fs');
+
+// Get '@monax/legacy-contracts'. 
+var contracts = require('@monax/legacy-contracts');
+ 
+// Get '@monax/legacy-db' (the javascript API for Burrow) 
+var burrowModule = require("@monax/legacy-db");
+ 
+// Create a new instance of Burrow that uses the given URL. 
+var burrow = burrowModule.createInstance("http://localhost:1337/rpc");
+// The private key. 
+var accountData = require('./accounts.json');
+ 
+// Create a new pipe. 
+var pipe = new contracts.pipes.DevPipe(burrow, accountData.simplechain_full_000);
+// Create a new contracts object using that pipe. 
+var contractManager = contracts.newContractManager(pipe);
+
+//source of the smart contract
+const myJsonAbi = JSON.parse(fs.readFileSync("./MyContract.abi", 'utf8'));
+const myCode = fs.readFileSync("./MyContract.bin", 'utf8');
+
+// Create a factory (or contract template) from 'myJsonAbi' 
+var myContractFactory = contractManager.newContractFactory(myJsonAbi);
+
+var address = "A019878F1CC0B94F5FE34EAB45038344D3FE7187";
+var myContract = myContractFactory.at(address);
+
+var res;
+ 
+try{
+    myContract.add(3, 2, function (error,result){
+        if (error) throw error;
+        console.log(result.toNumber());
+    });
+} catch (error) {
+    console.log(error);
+}
---
+++ tolik/multinode/GSFactory/get.js
@@ -0,0 +1,40 @@
+// requires
+var fs = require ('fs');
+
+// Get '@monax/legacy-contracts'. 
+var contracts = require('@monax/legacy-contracts');
+ 
+// Get '@monax/legacy-db' (the javascript API for Burrow) 
+var burrowModule = require("@monax/legacy-db");
+ 
+// Create a new instance of Burrow that uses the given URL. 
+var burrow = burrowModule.createInstance("http://192.168.33.10:1337/rpc");
+// The private key. 
+var accountData = require('./accounts.json');
+ 
+// Create a new pipe. 
+var pipe = new contracts.pipes.DevPipe(burrow, accountData.multichain_full_000);
+// Create a new contracts object using that pipe. 
+var contractManager = contracts.newContractManager(pipe);
+
+//source of the smart contract
+const myJsonAbi = JSON.parse(fs.readFileSync("./abi/GSContract", 'utf8'));
+
+// Create a factory (or contract template) from 'myJsonAbi' 
+var myContractFactory = contractManager.newContractFactory(myJsonAbi);
+
+// 'monax pkgs do' output file
+var json = JSON.parse(fs.readFileSync("./jobs_output.json",'utf8'));
+var address = json.getGSAddr; // or use get_addr.js
+var myContract = myContractFactory.at(address);
+
+var res;
+ 
+try{
+    myContract.get(function (error,result){
+        if (error) throw error;
+        console.log(result.toNumber());
+    });
+} catch (error) {
+    console.log(error);
+}
